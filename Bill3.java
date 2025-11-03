d7574e74dd65f3211ab86981b0aef74e61d8ce6d
David Shfaim <davidshfaim@MacBook-Pro-sl-David.local>
2025-11-03 13:39:02 +0200

    fix: first try

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		double amount1 = Double.parseDouble(args[3]);
+		double split = Math.ceil(amount1 / 3);
+		System.out.println ("dear" + " " + name3 + ", " + name2 + " and " + name1 + " :" + " pay " + split + " shekels" + " ecah.");
+		                    
+
 	    // Replace this comment with the rest of your code   
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,14 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		String currentvalue = args[0];
+		String rate = args[1];
+		String years = args[2];
+		double cv = Double.parseDouble(currentvalue);
+		double y = Double.parseDouble(years);
+		double r = Double.parseDouble(rate);
+		double fv = cv * Math.pow((1 + r/100), y);
+		System.out.println("after " + years + " years, " + cv + "$ " + " saved at " + rate + "% " + " will yield " + (int) fv + "$");	
+	
 	}
 }
