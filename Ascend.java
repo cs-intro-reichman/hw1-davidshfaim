1ecb17a81140e8dbf2ab18906e2bdd846c26ae6c
David Shfaim <davidshfaim@MacBook-Pro-sl-David.local>
2025-11-04 23:06:48 +0200

    fix a n 1

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,19 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		String number = args[0];
+		double num = Double.parseDouble(number);
+		double ran1 = Math.random() ;
+		double ran2 = Math.random() ;
+		double ran3 = Math.random() ;
+		double a = ran1 * num + 1; 
+		double b = ran2 * num + 1;
+		double c = ran3 * num + 1;
+		System.err.println((int)a + " " +(int)b + " " + (int)c);
+        double max = Math.max(a , Math.max(b, c));
+		double min = Math.min(a , Math.min(b, c));
+		double even = (a + b + c) - (max + min);
+		System.out.println((int)min + " " +(int)even + " " + (int)max);
+
 	}
 }
